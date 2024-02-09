8eb192ae5f50c141c31c8732ed7a1ac26e2a6502
Tusharkuma k <tusharkumar121001@gmail.com>
2024-02-09 09:39:50 +0000

    test file

---
+++ test/fundmetest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+import {test} from "./lib/forge-std/src/Test.sol";
+contract fundmetest {
+    function setup() external {}
+
+    function testdemo() external {}
+}
