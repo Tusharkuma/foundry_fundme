08f40fe2d145e33356e80d71eb08a8a6c8a80b92
Tusharkuma k <tusharkumar121001@gmail.com>
2024-02-19 16:32:58 +0000

    working on test file

--- test/fundmetest.t.sol
+++ test/fundmetest.t.sol
@@ -15,7 +15,15 @@ contract fundmetest is Test,FundMe {
     function testdemo() external {
         // assertEq(number,2);
         // console.log(number);
-        
+        assertEq(fundme.MINIMUM_USD(),5e18);
 
+    }
+
+    function testsendisowner() public {
+        console.log(fundme.i_owner());  //0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496
+        console.log(msg.sender);        //0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38
+        console.log(address(this));     //0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496
+             
+        assertEq(fundme.i_owner(),address(this));
     }
 }
