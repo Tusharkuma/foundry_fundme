6cc1a9f7b3702fe58451cf799881ead9fdedbf33
Tusharkuma k <tusharkumar121001@gmail.com>
2024-02-21 11:45:30 +0000

    1st commit,working on script file

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract DeployFundMe {
+
+}
